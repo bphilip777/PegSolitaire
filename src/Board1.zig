2000d523b5a1cb399d4168a14a631f63bbbb3e01
IdiotAtWork <bphilip777@gmail.com>
2025-08-18 18:54:54 -0600

    Added in abliity to init game with specific board state

--- src/Board1.zig
+++ src/Board1.zig
@@ -25,16 +25,11 @@ fn invTriNum(n: T) T {
 }
 
 test "Inv Tri Num" {
-    // 0
-    // 1 2
-    // 3 4 5
-    // 6 7 8 9
-    // 10 11 12 13
     //     0
     //    1 2
     //   3 4 5
     //  6 7 8 9
-    // 0 1 2 3  4
+    // 0 1 2 3 4
     const inputs = [_]T{ 0, 1, 3, 5, 6, 10 };
     const answers = [_]T{ 0, 1, 2, 2, 3, 4 };
     for (inputs, answers) |input, answer| {
@@ -200,7 +195,6 @@ pub fn createBoard(comptime n_rows: T) !type {
 
             // Initial moves
             self.updateNegMove(start);
-            self.updatePosMove();
 
             return self;
         }
