312aefeca0489acd600f2241deb94bb0b53c1649
IdiotAtWork <bphilip777@gmail.com>
2025-08-21 17:25:29 -0600

    Added in win and lose tests

--- src/main.zig
+++ src/main.zig
@@ -2,44 +2,17 @@ const std = @import("std");
 const print = std.debug.print;
 const Allocator = std.mem.Allocator;
 
-const createBoard = @import("Board1.zig").createBoard;
+const createBoard = @import("Board.zig").createBoard;
 const N_ROWS = 5;
 const Board: type = createBoard(N_ROWS) catch unreachable;
-const Direction = @import("Board1.zig").Direction;
+const Direction = @import("Board.zig").Direction;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
     const allo = gpa.allocator();
     defer std.debug.assert(gpa.deinit() == .ok);
 
-    // test win/loss conditions
-    const start = 0;
-    var board: Board = try .init(allo, start);
-    defer board.deinit();
-    board.printBoard();
-
-    const Instruction = struct { idx: u16, dir: Direction };
-    const list_of_instructions = [_]Instruction{
-        .{ .idx = 0, .dir = .DownLeft },
-        .{ .idx = 3, .dir = .Right },
-        .{ .idx = 5, .dir = .UpLeft },
-        .{ .idx = 1, .dir = .DownLeft },
-        .{ .idx = 2, .dir = .DownRight },
-        .{ .idx = 3, .dir = .DownRight },
-        .{ .idx = 0, .dir = .DownLeft },
-        .{ .idx = 5, .dir = .UpLeft },
-        .{ .idx = 12, .dir = .Left },
-        .{ .idx = 11, .dir = .Right },
-        .{ .idx = 12, .dir = .UpRight },
-        .{ .idx = 10, .dir = .Right },
-    };
-    for (list_of_instructions) |instruction| {
-        board.chooseMove(instruction.idx, instruction.dir);
-    }
-    board.printBoard();
-    try board.printMoves();
-    print("{}\n", .{board.isLost()});
+    _ = allo;
 }
 
 // test "Lose Condition" {
@@ -47,11 +20,59 @@ pub fn main() !void {
 // }
 
 test "Run All Tests" {
-    _ = @import("Board1.zig");
+    _ = @import("Board.zig");
 }
 
-// 0
-// 1 2
-// 3 4 5
-// 6 7 8 9
-// 0 1 2 3 4
+test "Is Lost" {
+    const allo = std.testing.allocator;
+    var board: Board = try .init(allo, 0);
+    defer board.deinit();
+
+    const Instruction = struct { idx: u16, dir: Direction, is_lost: bool };
+    const list_of_instructions = [_]Instruction{
+        .{ .idx = 0, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 3, .dir = .Right, .is_lost = false },
+        .{ .idx = 5, .dir = .UpLeft, .is_lost = false },
+        .{ .idx = 1, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 2, .dir = .DownRight, .is_lost = false },
+        .{ .idx = 3, .dir = .DownRight, .is_lost = false },
+        .{ .idx = 0, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 5, .dir = .UpLeft, .is_lost = false },
+        .{ .idx = 12, .dir = .Left, .is_lost = false },
+        .{ .idx = 11, .dir = .Right, .is_lost = false },
+        .{ .idx = 12, .dir = .UpRight, .is_lost = false },
+        .{ .idx = 10, .dir = .Right, .is_lost = true },
+    };
+
+    for (list_of_instructions) |instruction| {
+        board.chooseMove(instruction.idx, instruction.dir);
+        try std.testing.expectEqual(board.isLost(), instruction.is_lost);
+    }
+}
+
+test "Is Won" {
+    const allo = std.testing.allocator;
+    var board: Board = try .init(allo, 0);
+    defer board.deinit();
+
+    const Instruction = struct { idx: u16, dir: Direction, is_lost: bool };
+    const list_of_instructions = [_]Instruction{
+        .{ .idx = 0, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 3, .dir = .Right, .is_lost = false },
+        .{ .idx = 5, .dir = .UpLeft, .is_lost = false },
+        .{ .idx = 1, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 2, .dir = .DownRight, .is_lost = false },
+        .{ .idx = 3, .dir = .DownRight, .is_lost = false },
+        .{ .idx = 0, .dir = .DownLeft, .is_lost = false },
+        .{ .idx = 5, .dir = .UpLeft, .is_lost = false },
+        .{ .idx = 12, .dir = .Left, .is_lost = false },
+        .{ .idx = 11, .dir = .Right, .is_lost = false },
+        .{ .idx = 12, .dir = .UpRight, .is_lost = false },
+        .{ .idx = 10, .dir = .Right, .is_lost = true },
+    };
+
+    for (list_of_instructions) |instruction| {
+        board.chooseMove(instruction.idx, instruction.dir);
+        try std.testing.expectEqual(board.isLost(), instruction.is_lost);
+    }
+}
